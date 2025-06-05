01ada7de0bddaf01ff8d44cf8e89faa6f126c0fa
Nguyen Gia Huy <ghgaming1234563@gmail.com>
2025-06-05 18:14:54 +0700

    do the add transaction

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,18 +1,91 @@
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
-
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { Dropdown } from "react-native-element-dropdown";
 const AddTransaction = () => {
     const [service1, setService1] = useState(false);
     const [service2, setService2] = useState(false);
     const [service3, setService3] = useState(false);
+    const [value, setValue] = useState("");
+    const data = [
+        { label: 'Executor', value: '1' }
+    ]
     return (
         <View style={styles.container}>
-            <Text style={styles.bold}>Customer*</Text>
-            <View style={styles.checkboxSection}>
-                
-                <Text style={styles.text}>Lột mụn</Text>
+            <View style={styles.section}>
+                <Text style={styles.bold}>Customer*</Text>
+                <View style={styles.selectSection}>
+                    <Dropdown
+                        style={styles.dropdown}
+                        data={data}
+                        placeholder="Select Service"
+                        onChange={(item) => console.log(item)}
+                        valueField={"value"}
+                        labelField={"label"}
+                        value={value}
+                    />
+                </View>
             </View>
 
+            <View style={styles.section}>
+                <View style={styles.checkboxSection}>
+                    <BouncyCheckbox
+                        onPress={() => setService1(!service1)}
+                    />
+                    <Text style={styles.text}>Lột mụn</Text>
+                </View>
+                <View style={styles.selectSection}>
+                    <Dropdown
+                        style={styles.dropdown}
+                        data={data}
+                        placeholder="Select Service"
+                        onChange={(item) => console.log(item)}
+                        valueField={"value"}
+                        labelField={"label"}
+                        value={value}
+                    />
+                </View>
+                <Text style={styles.redText}>Price: 40.000d</Text>
+            </View>
+
+            <View style={styles.section}>
+                <View style={styles.checkboxSection}>
+                    <BouncyCheckbox
+                        onPress={() => setService1(!service1)}
+                    />
+                    <Text style={styles.text}>Lột mụn</Text>
+                </View>
+                <View style={styles.selectSection}>
+                    <Dropdown
+                        style={styles.dropdown}
+                        data={data}
+                        placeholder="Select Service"
+                        onChange={(item) => console.log(item)}
+                        valueField={"value"}
+                        labelField={"label"}
+                        value={value}
+                    />
+                </View>
+                <Text style={styles.redText}>Price: 40.000d</Text>
+            </View>
+            <View style={styles.checkboxSection}>
+                <BouncyCheckbox
+                    onPress={() => setService1(!service1)}
+                />
+                <Text style={styles.text}>Lột mụn</Text>
+            </View>
+            <View style={styles.checkboxSection}>
+                <BouncyCheckbox
+                    onPress={() => setService1(!service1)}
+                />
+                <Text style={styles.text}>Lột mụn</Text>
+            </View>
+            <View style={styles.checkboxSection}>
+                <BouncyCheckbox
+                    onPress={() => setService1(!service1)}
+                />
+                <Text style={styles.text}>Lột mụn</Text>
+            </View>
         </View>
     )
 }
@@ -28,11 +101,32 @@ const styles = new StyleSheet.create({
         fontSize: 18,
         marginBottom: 10,
     },
-    checkboxSection:{
+    checkboxSection: {
         flexDirection: "row",
     },
-    text:{
+    text: {
         color: '#333',
         marginTop: 5,
+    },
+    selectSection: {
+        flexDirection: "row",
+        marginTop: 10,
+    },
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        flex: 1,
+    },
+    redText: {
+        color: '#EF506B',
+        fontWeight: 'bold',
+    },
+    section: {
+        marginBottom: 20,
+        flexDirection: "column",
     }
+
 })
